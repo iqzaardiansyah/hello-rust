d63ea98d103ce19368fca32dad36b9b78c8cef51
iqzaardiansyah <iqzaardiansyah@gmail.com>
2024-03-22 17:14:43 +0700

    Modify code so it can be run from terminal launch on root directory

--- src/main.rs
+++ src/main.rs
@@ -36,6 +36,8 @@ fn handle_connection(mut stream: TcpStream) {
             _ => ("HTTP/1.1 404 NOT FOUND", "bad.html"),
         };
 
+        let filename = format!("src/templates/{}", filename);
+
         if let Ok(contents) = fs::read_to_string(filename) {
             let length = contents.len();
             let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
